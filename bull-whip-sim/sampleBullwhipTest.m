d4004c20952e19cc4a5447a51ff6f82543b83b40
billtubbs <bill.tubbs@me.com>
2023-09-28 10:09:17 -0700

    Updated code from Bhagyesh

--- bull-whip-sim/sampleBullwhipTest.m
+++ bull-whip-sim/sampleBullwhipTest.m
@@ -1,74 +1,75 @@
+%%
+%% Code to simulate Beer Game for supply chain with three echelons 
+%% Bhagyesh Patil, IFAC Indsutry Task Force 
+%% Last modified, 28/09/2023
+%%
 
-function y =sampleBullwhipTest() 
-    % Beer Game simulation and Bullwhip effect with EOQ model  
-    % clear; close all; clc;  
-      
-    % Parameters  
-    n_weeks = 52;  % Number of weeks  
-    lead_time = 2; % Lead time in weeks  
-    n_players = 3; % Number of players in the supply chain (retailer, wholesaler, manufacturer)  
-    init_inventory = 12; % Initial inventory for each player  
-    init_demand = 4; % Initial demand  
-    holding_cost = 10; % Holding cost per unit per week  
-    order_cost = 5; % Order cost per order  
-      
-    % Random demand fluctuations  
-    rng(0); % Seed for reproducibility  
-
-    % Steady increase in demand for first 10 samples
-    Nd=1; N=10;
-    for i=2:N
-        Nd(:,i) = Nd(:,i-1) + 0.1;
-    end
-    % After 10 samples random demand generation to replicate Bullwhip effect
-    demand_fluctuation = [Nd 2*(randi([0 1], 1, n_weeks-length(Nd)) - 0.5)];   
-      
-    % Initialize variables  
-    demand = init_demand * ones(1, n_weeks) + demand_fluctuation;  
-    orders = zeros(n_players, n_weeks);  
-    inventory = init_inventory * ones(n_players, n_weeks);  
-      
-     
-    % Beer Game simulation  
-    for t = 2:n_weeks  
-        for p = 1:n_players  
-            if p == 1  
-                orders(p, t) = demand(t - 1);  
-            else  
-                orders(p, t) = eoq_order(inventory(p - 1, t - 1), holding_cost, order_cost);  
-            end  
-              
-            if t > lead_time  
-                incoming_orders = orders(p, t - lead_time);  
-            else  
-                incoming_orders = init_demand;  
-            end  
-              
-            inventory(p, t) = inventory(p, t - 1) - orders(p, t) + incoming_orders;  
-        end  
-    end  
-    
-    % Plot results  
-    figure;  
-    plot(1:n_weeks, demand, 'LineWidth', 2);  
-    hold on;  
-    plot(1:n_weeks, inventory', 'LineWidth', 1.5);  
-    xlabel('Week');  
-    ylabel('Inventory Level');  
-    title('Beer Game Simulation with EOQ Model and Bullwhip Effect');  
-    legend('Customer Demand', 'Retailer', 'Wholesaler', 'Manufacturer', 'Location', 'NorthWest');  
-    grid on;  
-end
-
+% Beer Game simulation and Bullwhip effect with non-negative orders and different lead times  
+clear;   
+close all;   
+clc;  
   
-% EOQ order function  
-function order_quantity = eoq_order(demand, holding_cost, order_cost)  
-    order_quantity = sqrt((2 * demand * order_cost) / holding_cost);  
-end 
+% Parameters    
+n_weeks = 52;                   % Number of weeks    
+n_players = 3;                  % Number of players in the supply chain    
+init_inventory = [20, 10, 30];  % Initial inventory for each player    
+lead_times = [1, 3, 1];         % Lead time for each player  
+  
+% % demand = 4;                 % Constant demand  
+N=5;
+% demand = [1*ones(1,N) 3*ones(1,n_weeks-N)];
+% demand = [1*ones(1,N) 5*ones(1,2*N) 3*ones(1,n_weeks-(N + 2*N))];
+demand = [1*ones(1,N) 5*ones(1,2*N) 2*ones(1,3*N) 3*ones(1,n_weeks-(N + 2*N + 3*N))];
+
+% Initialize variables    
+orders = zeros(n_players, n_weeks);    
+inventory = zeros(n_players, n_weeks);    
+inventory(:, 1) = init_inventory';    
+    
+% Beer Game simulation    
+for t = 2:n_weeks    
+    for p = 1:n_players    
+        if p == 1    
+            orders(p, t) = demand(t);    
+        else    
+            orders(p, t) = max(0, inventory(p - 1, t - 1));    
+        end    
+            
+        if t > lead_times(p)    
+            incoming_orders = orders(p, t - lead_times(p));    
+        else    
+            incoming_orders = demand(t);    
+        end    
+            
+        inventory(p, t) = max(0, inventory(p, t - 1) - orders(p, t) + incoming_orders);           
+    end    
+end    
+    
+% Plot results    
+figure
+plot(1:n_weeks, demand, 'LineWidth', 2);    
+xlabel('Week');    
+ylabel('Demand Value');    
+title('Demand Profile');    
+legend('Customer Demand');    
+
+figure;    
+% plot(1:n_weeks, demand, 'LineWidth', 2);    
+% hold on;    
+plot(1:n_weeks, orders', 'LineWidth', 1.5);    
+xlabel('Week');    
+ylabel('Order Quantity');    
+title('Ordering Policy Profile');    
+legend('Customer Demand', 'Retailer', 'Wholesaler', 'Manufacturer');    
+% grid on;  
 
 
-
-
-
-
-
+figure;    
+% plot(1:n_weeks, demand, 'LineWidth', 2);    
+% hold on;    
+plot(1:n_weeks, inventory', 'LineWidth', 1.5);    
+xlabel('Week');    
+ylabel('Inventory Value');    
+title('Inventory Levels');    
+legend('Customer Demand', 'Retailer', 'Wholesaler', 'Manufacturer');    
+% grid on;  
